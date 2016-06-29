bfa017c87b37ebc82e3ed8bcbe36c072c6805a33
Erhu Akpobaro <erhuakpobaro@yahoo.co.uk>
2016-06-29 23:19:54 +0100

    comments

--- musicbrain.php
+++ musicbrain.php
@@ -39,7 +39,7 @@
 				  }
 				 }
 				 
-				function addgenre() { // adding genres
+				function addgenre() { // adding genress
 				$this->load->database();
 						$username = $_POST['Username'];
 						$user_token = $_POST['user_token'];
